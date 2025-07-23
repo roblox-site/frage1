b445a0fd9edea99563e9ae73bdcbb13b15048dc4
bestscams <bestscamsholder@gmail.com>
2025-07-23 07:11:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// script.js
 
+document.addEventListener('DOMContentLoaded', () => {
+  const noDiscord = document.getElementById('no-discord');
+  const discordWrapper = document.querySelector('.discord-input-wrapper');
+
+  noDiscord.addEventListener('change', () => {
+    // Nur das Textfeld verbergen/anzeigen
+    discordWrapper.classList.toggle('hidden', noDiscord.checked);
+  });
+});
